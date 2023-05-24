fe61fefc80b26dd503439ac6d8487cece0a36451
rifkkimaulana <rifkkimaulana@gmail.com>
2023-05-24 16:09:46 +0700

    Edit halaman kategori

--- admin/kategori/proses_simpan.php
+++ admin/kategori/proses_simpan.php
@@ -3,22 +3,16 @@
 
     $kategori_post = $_POST['kategori_post'];
 
-    
-    //simpan data ke database
     $insert = mysqli_query($koneksi, "INSERT INTO kategori VALUES (
         NULL,
         '$kategori_post'
     )");
 
-    //cek apakah proses simpan ke database berhasil
     if($insert){
-        //jika berhasil tampilkan pesan berhasil simpan data
         echo "<script>
-            alert('Data Berhasil Ditambahkan');
             window.location.href='index.php';
         </script>";
     }else{
-        //jika gagal tampilkan pesan gagal simpan data
         echo "<script>
             alert('Data Gagal Ditambahkan');
             window.location.href='index.php';

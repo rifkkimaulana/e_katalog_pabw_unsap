ebf303aa047e75ee296edc6cd07ba4428a95ddf5
rifkkimaulana <rifkkimaulana@gmail.com>
2023-05-24 15:10:58 +0700

    merubah koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,7 @@
 <?php
-//KONEKSI KE DATABASE
  $koneksi = mysqli_connect("localhost", "root", "", "e-katalog");
  
  if(!$koneksi){
  die("Connection failed: " . mysqli_connect_error());
  }
- echo "koneksi berhasil";
 ?>
